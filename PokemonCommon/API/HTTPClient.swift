2c28d2d0431a10347086c2f64e0b995920d10537
Andres Arana <3av.arana@gmail.com>
2023-03-14 12:25:33 -0400

    Create basic `HTTPClient` and `HTTPClientTask` protocols

---
+++ PokemonCommon/API/HTTPClient.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPClient.swift
+//  PokemonCommon
+//
+//  Created by Victor Arana on 3/14/23.
+//
+
+import Foundation
+
+public protocol HTTPClientTask {
+    func cancel()
+}
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask
+}
---
+++ PokemonCommon/API/URLSessionHTTPClient.swift
@@ -0,0 +1,8 @@
+//
+//  URLSessionHTTPClient.swift
+//  PokemonCommon
+//
+//  Created by Victor Arana on 3/14/23.
+//
+
+import Foundation
