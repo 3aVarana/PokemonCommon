36152b786a649a34f8f4117aae7249435d2fb411
Andres Arana <3av.arana@gmail.com>
2023-03-14 12:21:51 -0400

    Initial Commit

---
+++ PokemonCommon/PokemonCommon.h
@@ -0,0 +1,18 @@
+//
+//  PokemonCommon.h
+//  PokemonCommon
+//
+//  Created by Victor Arana on 3/14/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokemonCommon.
+FOUNDATION_EXPORT double PokemonCommonVersionNumber;
+
+//! Project version string for PokemonCommon.
+FOUNDATION_EXPORT const unsigned char PokemonCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokemonCommon/PublicHeader.h>
+
+
