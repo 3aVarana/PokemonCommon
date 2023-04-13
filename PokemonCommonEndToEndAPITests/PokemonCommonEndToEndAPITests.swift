e2dfa25ea0507a4590dbac0610e5f951ebf0c9a4
Andres Arana <3av.arana@gmail.com>
2023-04-13 11:56:34 -0400

    Test `endToEnd` server GET result matches fixed test account data

--- PokemonCommonEndToEndAPITests/PokemonCommonEndToEndAPITests.swift
+++ PokemonCommonEndToEndAPITests/PokemonCommonEndToEndAPITests.swift
@@ -14,7 +14,15 @@ final class PokemonCommonEndToEndAPITests: XCTestCase {
         let result = getResult()
         switch result {
         case let .success(pokemonList)?:
-            XCTAssertEqual(pokemonList.count, 12)
+            XCTAssertEqual(pokemonList.count, 12, "Expected 12 Pokemon in the list for the the account")
+            XCTAssertEqual(pokemonList[0], expectedPokemon(at: 0))
+            XCTAssertEqual(pokemonList[1], expectedPokemon(at: 1))
+            XCTAssertEqual(pokemonList[2], expectedPokemon(at: 2))
+            XCTAssertEqual(pokemonList[3], expectedPokemon(at: 3))
+            XCTAssertEqual(pokemonList[4], expectedPokemon(at: 4))
+            XCTAssertEqual(pokemonList[5], expectedPokemon(at: 5))
+            XCTAssertEqual(pokemonList[6], expectedPokemon(at: 6))
+            XCTAssertEqual(pokemonList[7], expectedPokemon(at: 7))
         case let .failure(error)?:
             XCTFail("Expected successful Pokemon list, got \(error) instead")
         default:
@@ -23,7 +31,9 @@ final class PokemonCommonEndToEndAPITests: XCTestCase {
         
     }
     
-    func getResult(file: StaticString = #file, line: UInt = #line) -> RemotePokemonLoader.Result? {
+    // MARK: - Helpers
+    
+    private func getResult(file: StaticString = #file, line: UInt = #line) -> RemotePokemonLoader.Result? {
         let testServerURL = URL(string: "https://stoplight.io/mocks/pokedex/pokeapi/158224127/pokemon")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemotePokemonLoader(url: testServerURL, client: client)
@@ -42,6 +52,67 @@ final class PokemonCommonEndToEndAPITests: XCTestCase {
         wait(for: [exp], timeout: 5.0)
         
         return receivedResult
+    }
+    
+    private func expectedPokemon(at index: Int) -> Pokemon {
+        return Pokemon(id: id(at: index),
+                       name: name(at: index),
+                       url: url(at: index),
+                       imageUrl: imageURL(at: index),
+                       types: types(at: index))
+    }
+    
+    private func id(at index: Int) -> Int {
+        return index + 1
+    }
+    
+    
+    private func name(at index: Int) -> String {
+        return [
+            "bulbasaur",
+            "ivysaur",
+            "venusaur",
+            "charmander",
+            "charmaleon",
+            "charizard",
+            "squirtle",
+            "wartortle",
+            "blastoise",
+            "caterpie",
+            "metapod",
+            "butterfree",
+        ][index]
+    }
+    
+    private func url(at index: Int) -> URL {
+        return URL(string: "https://pokeapi.co/api/v2/pokemon/\(index+1)")!
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/\(index+1).png")!
+    }
+    
+    private func types(at index: Int) -> [PokemonType] {
+        return [[PokemonType(slot: 1, code: 12, name: "grass"),
+                 PokemonType(slot: 2, code: 4, name: "poison")],
+                [PokemonType(slot: 1, code: 12, name: "grass"),
+                 PokemonType(slot: 2, code: 4, name: "poison")],
+                [PokemonType(slot: 1, code: 12, name: "grass"),
+                 PokemonType(slot: 2, code: 4, name: "poison")],
+                
+                [PokemonType(slot: 1, code: 10, name: "fire")],
+                [PokemonType(slot: 1, code: 10, name: "fire")],
+                [PokemonType(slot: 1, code: 10, name: "fire"),
+                 PokemonType(slot: 2, code: 3, name: "flying")],
+                
+                [PokemonType(slot: 1, code: 11, name: "water")],
+                [PokemonType(slot: 1, code: 11, name: "water")],
+                [PokemonType(slot: 1, code: 11, name: "water")],
+                
+                [PokemonType(slot: 1, code: 7, name: "bug")],
+                [PokemonType(slot: 1, code: 7, name: "bug")],
+                [PokemonType(slot: 1, code: 7, name: "bug"),
+                 PokemonType(slot: 2, code: 3, name: "flying")]][index]
     }
 
 }
