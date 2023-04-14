b8400d3c941274ad66edba980d4952ac15076ecc
Andres Arana <3av.arana@gmail.com>
2023-04-13 21:19:05 -0400

    Create new target for iOS project

---
+++ Pokemon iOS/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon iOS
+//
+//  Created by Victor Arana on 4/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
